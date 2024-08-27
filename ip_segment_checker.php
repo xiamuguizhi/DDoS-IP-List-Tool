518b51f80696a397bf7dae7c9b1e86abd529d145
夏目贵志 <19650297+xiamuguizhi@users.noreply.github.com>
2024-08-27 17:48:47 +0800

    Update v1.0

--- ip_segment_checker.php
+++ ip_segment_checker.php
@@ -3,7 +3,7 @@
 /**
  * @file        ddos-ip-handler.php
  * @description 读取并处理包含恶意攻击IP地址的文件，提取并输出其中的IP地址。
- * @author      [夏目贵志/https://github.com/xiamuguizhi/]
+ * @author      夏目贵志 / https://github.com/xiamuguizhi/
  * @date        2024-08-27
  */
 
